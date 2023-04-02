415960e740fb6699cccf8f67927c01772e819937
Deepak Chaula <deepak.chaula@maracana.in>
2023-04-03 02:22:12 +0530

    update-card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import './App.css';
 import Header from "./components/Header";
 import { getCategories } from "./utils/categories";
 import axios from "axios";
 import { Post } from "./models/post";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 const CategoryTree = React.lazy(() => import("./components/CategoryTree"));
 // const SubcategoryNews = React.lazy(() => import("./components/CategoryTree"));
@@ -12,6 +16,7 @@ const CategoryTree = React.lazy(() => import("./components/CategoryTree"));
 function App() {
   const [posts, setPosts] = React.useState<Post[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     const fetchPostsAsync = async () => {
@@ -28,15 +33,35 @@ function App() {
 
   const categories = getCategories(posts);
 
+  const backToRoot = () => {
+    navigate("/");
+  }
+
   type SubcategoryNewsProps = {
     posts: Post[];
   }
-  
+
   const SubcategoryNews: React.FC<SubcategoryNewsProps> = ({ posts }) => {
     return (
-      <div>
+      <div className='main-wrapper'>
         {posts.map((post) => (
-          <div key={post.id}>{post.title}</div>
+          <Card sx={{ maxWidth: 345, m: 1 }} key={post.id} >
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {post.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {post.body}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary" onClick={backToRoot}>
+                Read later
+              </Button>
+            </CardActions>
+          </Card>
         ))}
       </div>
     );
